d46f755befd760190e4bc13bcf9bd18f2e3038fd
Ahmet Celikezer <ahmet@celikezer.com>
2022-06-11 19:05:24 +0300

    add mikro-orm module & configurations

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,22 @@
+import { MikroOrmModuleOptions as Options } from '@mikro-orm/nestjs';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { LoadStrategy } from '@mikro-orm/core';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+
+const config: Options = {
+  type: 'postgresql',
+  clientUrl: process.env.DATABASE_URL,
+  entities: ['dist/**/entity/*.entity.js'],
+  entitiesTs: ['src/**/entity/*.entity.ts'],
+  debug: process.env.NODE_ENV !== 'production',
+  loadStrategy: LoadStrategy.JOINED,
+  highlighter: new SqlHighlighter(),
+  metadataProvider: TsMorphMetadataProvider,
+  registerRequestContext: false,
+  migrations: {
+    path: 'dist/migrations',
+    pathTs: 'src/migrations',
+  },
+};
+
+export default config;
