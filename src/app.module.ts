bf1226fb146e245728cb4921904ad59ae72e3e90
Ahmet Celikezer <ahmet@celikezer.com>
2022-06-11 19:03:15 +0300

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV || 'development';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV === 'production' ? '.env' : `.env.${ENV}`,
+    }),
+  ],
 })
 export class AppModule {}
