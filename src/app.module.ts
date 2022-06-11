8f8d57bbd946d29b18c4688c03e3ef5da5a5aab2
Ahmet Celikezer <ahmet@celikezer.com>
2022-06-11 19:11:33 +0300

    register mikro-orm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,11 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  OnModuleInit,
+} from '@nestjs/common';
+import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroORM } from '@mikro-orm/core';
 import { ConfigModule } from '@nestjs/config';
 
 const ENV = process.env.NODE_ENV || 'development';
@@ -9,6 +16,17 @@ const ENV = process.env.NODE_ENV || 'development';
       isGlobal: true,
       envFilePath: ENV === 'production' ? '.env' : `.env.${ENV}`,
     }),
+    MikroOrmModule.forRoot(),
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule, OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit(): Promise<void> {
+    await this.orm.getMigrator().up();
+  }
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MikroOrmMiddleware).forRoutes('*');
+  }
+}
