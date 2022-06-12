a39978a2d6c2a4544a3cf9def07d75e4cde9a653
Ahmet Celikezer <ahmet@celikezer.com>
2022-06-12 10:19:11 +0300

    change value range of wallet balance factory

--- src/user/factory/entity/wallet.factory.ts
+++ src/user/factory/entity/wallet.factory.ts
@@ -7,7 +7,7 @@ export class WalletFactory extends Factory<Wallet> {
   protected definition(faker: Faker): Partial<Wallet> {
     return {
       id: faker.datatype.uuid(),
-      balance: faker.finance.amount(0.01, 400.99, 2),
+      balance: faker.finance.amount(500, 50000.99, 2),
     };
   }
 }
